e613e8afa1f939b4d7be53f46f528ae1130877e6
mreiger <michael@rauschpfeife.net>
2023-05-22 21:04:06 +0200

    Generate mock Ensurer

--- extensions/pkg/webhook/controlplane/genericmutator/mock/mocks.go
+++ extensions/pkg/webhook/controlplane/genericmutator/mock/mocks.go
@@ -6,8 +6,6 @@ package mock
 
 import (
 	context "context"
-	reflect "reflect"
-
 	semver "github.com/Masterminds/semver"
 	unit "github.com/coreos/go-systemd/v22/unit"
 	v1alpha1 "github.com/gardener/etcd-druid/api/v1alpha1"
@@ -16,33 +14,35 @@ import (
 	gomock "github.com/golang/mock/gomock"
 	v1 "k8s.io/api/apps/v1"
 	v10 "k8s.io/api/core/v1"
+	v11 "k8s.io/autoscaler/vertical-pod-autoscaler/pkg/apis/autoscaling.k8s.io/v1"
 	v1beta1 "k8s.io/kubelet/config/v1beta1"
+	reflect "reflect"
 )
 
-// MockEnsurer is a mock of Ensurer interface.
+// MockEnsurer is a mock of Ensurer interface
 type MockEnsurer struct {
 	ctrl     *gomock.Controller
 	recorder *MockEnsurerMockRecorder
 }
 
-// MockEnsurerMockRecorder is the mock recorder for MockEnsurer.
+// MockEnsurerMockRecorder is the mock recorder for MockEnsurer
 type MockEnsurerMockRecorder struct {
 	mock *MockEnsurer
 }
 
-// NewMockEnsurer creates a new mock instance.
+// NewMockEnsurer creates a new mock instance
 func NewMockEnsurer(ctrl *gomock.Controller) *MockEnsurer {
 	mock := &MockEnsurer{ctrl: ctrl}
 	mock.recorder = &MockEnsurerMockRecorder{mock}
 	return mock
 }
 
-// EXPECT returns an object that allows the caller to indicate expected use.
+// EXPECT returns an object that allows the caller to indicate expected use
 func (m *MockEnsurer) EXPECT() *MockEnsurerMockRecorder {
 	return m.recorder
 }
 
-// EnsureAdditionalFiles mocks base method.
+// EnsureAdditionalFiles mocks base method
 func (m *MockEnsurer) EnsureAdditionalFiles(arg0 context.Context, arg1 context0.GardenContext, arg2, arg3 *[]v1alpha10.File) error {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "EnsureAdditionalFiles", arg0, arg1, arg2, arg3)
@@ -50,13 +50,13 @@ func (m *MockEnsurer) EnsureAdditionalFiles(arg0 context.Context, arg1 context0.
 	return ret0
 }
 
-// EnsureAdditionalFiles indicates an expected call of EnsureAdditionalFiles.
+// EnsureAdditionalFiles indicates an expected call of EnsureAdditionalFiles
 func (mr *MockEnsurerMockRecorder) EnsureAdditionalFiles(arg0, arg1, arg2, arg3 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "EnsureAdditionalFiles", reflect.TypeOf((*MockEnsurer)(nil).EnsureAdditionalFiles), arg0, arg1, arg2, arg3)
 }
 
-// EnsureAdditionalUnits mocks base method.
+// EnsureAdditionalUnits mocks base method
 func (m *MockEnsurer) EnsureAdditionalUnits(arg0 context.Context, arg1 context0.GardenContext, arg2, arg3 *[]v1alpha10.Unit) error {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "EnsureAdditionalUnits", arg0, arg1, arg2, arg3)
@@ -64,13 +64,13 @@ func (m *MockEnsurer) EnsureAdditionalUnits(arg0 context.Context, arg1 context0.
 	return ret0
 }
 
-// EnsureAdditionalUnits indicates an expected call of EnsureAdditionalUnits.
+// EnsureAdditionalUnits indicates an expected call of EnsureAdditionalUnits
 func (mr *MockEnsurerMockRecorder) EnsureAdditionalUnits(arg0, arg1, arg2, arg3 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "EnsureAdditionalUnits", reflect.TypeOf((*MockEnsurer)(nil).EnsureAdditionalUnits), arg0, arg1, arg2, arg3)
 }
 
-// EnsureClusterAutoscalerDeployment mocks base method.
+// EnsureClusterAutoscalerDeployment mocks base method
 func (m *MockEnsurer) EnsureClusterAutoscalerDeployment(arg0 context.Context, arg1 context0.GardenContext, arg2, arg3 *v1.Deployment) error {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "EnsureClusterAutoscalerDeployment", arg0, arg1, arg2, arg3)
@@ -78,13 +78,13 @@ func (m *MockEnsurer) EnsureClusterAutoscalerDeployment(arg0 context.Context, ar
 	return ret0
 }
 
-// EnsureClusterAutoscalerDeployment indicates an expected call of EnsureClusterAutoscalerDeployment.
+// EnsureClusterAutoscalerDeployment indicates an expected call of EnsureClusterAutoscalerDeployment
 func (mr *MockEnsurerMockRecorder) EnsureClusterAutoscalerDeployment(arg0, arg1, arg2, arg3 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "EnsureClusterAutoscalerDeployment", reflect.TypeOf((*MockEnsurer)(nil).EnsureClusterAutoscalerDeployment), arg0, arg1, arg2, arg3)
 }
 
-// EnsureETCD mocks base method.
+// EnsureETCD mocks base method
 func (m *MockEnsurer) EnsureETCD(arg0 context.Context, arg1 context0.GardenContext, arg2, arg3 *v1alpha1.Etcd) error {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "EnsureETCD", arg0, arg1, arg2, arg3)
@@ -92,13 +92,13 @@ func (m *MockEnsurer) EnsureETCD(arg0 context.Context, arg1 context0.GardenConte
 	return ret0
 }
 
-// EnsureETCD indicates an expected call of EnsureETCD.
+// EnsureETCD indicates an expected call of EnsureETCD
 func (mr *MockEnsurerMockRecorder) EnsureETCD(arg0, arg1, arg2, arg3 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "EnsureETCD", reflect.TypeOf((*MockEnsurer)(nil).EnsureETCD), arg0, arg1, arg2, arg3)
 }
 
-// EnsureKubeAPIServerDeployment mocks base method.
+// EnsureKubeAPIServerDeployment mocks base method
 func (m *MockEnsurer) EnsureKubeAPIServerDeployment(arg0 context.Context, arg1 context0.GardenContext, arg2, arg3 *v1.Deployment) error {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "EnsureKubeAPIServerDeployment", arg0, arg1, arg2, arg3)
@@ -106,13 +106,13 @@ func (m *MockEnsurer) EnsureKubeAPIServerDeployment(arg0 context.Context, arg1 c
 	return ret0
 }
 
-// EnsureKubeAPIServerDeployment indicates an expected call of EnsureKubeAPIServerDeployment.
+// EnsureKubeAPIServerDeployment indicates an expected call of EnsureKubeAPIServerDeployment
 func (mr *MockEnsurerMockRecorder) EnsureKubeAPIServerDeployment(arg0, arg1, arg2, arg3 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "EnsureKubeAPIServerDeployment", reflect.TypeOf((*MockEnsurer)(nil).EnsureKubeAPIServerDeployment), arg0, arg1, arg2, arg3)
 }
 
-// EnsureKubeAPIServerService mocks base method.
+// EnsureKubeAPIServerService mocks base method
 func (m *MockEnsurer) EnsureKubeAPIServerService(arg0 context.Context, arg1 context0.GardenContext, arg2, arg3 *v10.Service) error {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "EnsureKubeAPIServerService", arg0, arg1, arg2, arg3)
@@ -120,13 +120,13 @@ func (m *MockEnsurer) EnsureKubeAPIServerService(arg0 context.Context, arg1 cont
 	return ret0
 }
 
-// EnsureKubeAPIServerService indicates an expected call of EnsureKubeAPIServerService.
+// EnsureKubeAPIServerService indicates an expected call of EnsureKubeAPIServerService
 func (mr *MockEnsurerMockRecorder) EnsureKubeAPIServerService(arg0, arg1, arg2, arg3 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "EnsureKubeAPIServerService", reflect.TypeOf((*MockEnsurer)(nil).EnsureKubeAPIServerService), arg0, arg1, arg2, arg3)
 }
 
-// EnsureKubeControllerManagerDeployment mocks base method.
+// EnsureKubeControllerManagerDeployment mocks base method
 func (m *MockEnsurer) EnsureKubeControllerManagerDeployment(arg0 context.Context, arg1 context0.GardenContext, arg2, arg3 *v1.Deployment) error {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "EnsureKubeControllerManagerDeployment", arg0, arg1, arg2, arg3)
@@ -134,13 +134,13 @@ func (m *MockEnsurer) EnsureKubeControllerManagerDeployment(arg0 context.Context
 	return ret0
 }
 
-// EnsureKubeControllerManagerDeployment indicates an expected call of EnsureKubeControllerManagerDeployment.
+// EnsureKubeControllerManagerDeployment indicates an expected call of EnsureKubeControllerManagerDeployment
 func (mr *MockEnsurerMockRecorder) EnsureKubeControllerManagerDeployment(arg0, arg1, arg2, arg3 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "EnsureKubeControllerManagerDeployment", reflect.TypeOf((*MockEnsurer)(nil).EnsureKubeControllerManagerDeployment), arg0, arg1, arg2, arg3)
 }
 
-// EnsureKubeSchedulerDeployment mocks base method.
+// EnsureKubeSchedulerDeployment mocks base method
 func (m *MockEnsurer) EnsureKubeSchedulerDeployment(arg0 context.Context, arg1 context0.GardenContext, arg2, arg3 *v1.Deployment) error {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "EnsureKubeSchedulerDeployment", arg0, arg1, arg2, arg3)
@@ -148,13 +148,13 @@ func (m *MockEnsurer) EnsureKubeSchedulerDeployment(arg0 context.Context, arg1 c
 	return ret0
 }
 
-// EnsureKubeSchedulerDeployment indicates an expected call of EnsureKubeSchedulerDeployment.
+// EnsureKubeSchedulerDeployment indicates an expected call of EnsureKubeSchedulerDeployment
 func (mr *MockEnsurerMockRecorder) EnsureKubeSchedulerDeployment(arg0, arg1, arg2, arg3 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "EnsureKubeSchedulerDeployment", reflect.TypeOf((*MockEnsurer)(nil).EnsureKubeSchedulerDeployment), arg0, arg1, arg2, arg3)
 }
 
-// EnsureKubeletCloudProviderConfig mocks base method.
+// EnsureKubeletCloudProviderConfig mocks base method
 func (m *MockEnsurer) EnsureKubeletCloudProviderConfig(arg0 context.Context, arg1 context0.GardenContext, arg2 *semver.Version, arg3 *string, arg4 string) error {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "EnsureKubeletCloudProviderConfig", arg0, arg1, arg2, arg3, arg4)
@@ -162,13 +162,13 @@ func (m *MockEnsurer) EnsureKubeletCloudProviderConfig(arg0 context.Context, arg
 	return ret0
 }
 
-// EnsureKubeletCloudProviderConfig indicates an expected call of EnsureKubeletCloudProviderConfig.
+// EnsureKubeletCloudProviderConfig indicates an expected call of EnsureKubeletCloudProviderConfig
 func (mr *MockEnsurerMockRecorder) EnsureKubeletCloudProviderConfig(arg0, arg1, arg2, arg3, arg4 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "EnsureKubeletCloudProviderConfig", reflect.TypeOf((*MockEnsurer)(nil).EnsureKubeletCloudProviderConfig), arg0, arg1, arg2, arg3, arg4)
 }
 
-// EnsureKubeletConfiguration mocks base method.
+// EnsureKubeletConfiguration mocks base method
 func (m *MockEnsurer) EnsureKubeletConfiguration(arg0 context.Context, arg1 context0.GardenContext, arg2 *semver.Version, arg3, arg4 *v1beta1.KubeletConfiguration) error {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "EnsureKubeletConfiguration", arg0, arg1, arg2, arg3, arg4)
@@ -176,13 +176,13 @@ func (m *MockEnsurer) EnsureKubeletConfiguration(arg0 context.Context, arg1 cont
 	return ret0
 }
 
-// EnsureKubeletConfiguration indicates an expected call of EnsureKubeletConfiguration.
+// EnsureKubeletConfiguration indicates an expected call of EnsureKubeletConfiguration
 func (mr *MockEnsurerMockRecorder) EnsureKubeletConfiguration(arg0, arg1, arg2, arg3, arg4 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "EnsureKubeletConfiguration", reflect.TypeOf((*MockEnsurer)(nil).EnsureKubeletConfiguration), arg0, arg1, arg2, arg3, arg4)
 }
 
-// EnsureKubeletServiceUnitOptions mocks base method.
+// EnsureKubeletServiceUnitOptions mocks base method
 func (m *MockEnsurer) EnsureKubeletServiceUnitOptions(arg0 context.Context, arg1 context0.GardenContext, arg2 *semver.Version, arg3, arg4 []*unit.UnitOption) ([]*unit.UnitOption, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "EnsureKubeletServiceUnitOptions", arg0, arg1, arg2, arg3, arg4)
@@ -191,13 +191,13 @@ func (m *MockEnsurer) EnsureKubeletServiceUnitOptions(arg0 context.Context, arg1
 	return ret0, ret1
 }
 
-// EnsureKubeletServiceUnitOptions indicates an expected call of EnsureKubeletServiceUnitOptions.
+// EnsureKubeletServiceUnitOptions indicates an expected call of EnsureKubeletServiceUnitOptions
 func (mr *MockEnsurerMockRecorder) EnsureKubeletServiceUnitOptions(arg0, arg1, arg2, arg3, arg4 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "EnsureKubeletServiceUnitOptions", reflect.TypeOf((*MockEnsurer)(nil).EnsureKubeletServiceUnitOptions), arg0, arg1, arg2, arg3, arg4)
 }
 
-// EnsureKubernetesGeneralConfiguration mocks base method.
+// EnsureKubernetesGeneralConfiguration mocks base method
 func (m *MockEnsurer) EnsureKubernetesGeneralConfiguration(arg0 context.Context, arg1 context0.GardenContext, arg2, arg3 *string) error {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "EnsureKubernetesGeneralConfiguration", arg0, arg1, arg2, arg3)
@@ -205,13 +205,41 @@ func (m *MockEnsurer) EnsureKubernetesGeneralConfiguration(arg0 context.Context,
 	return ret0
 }
 
-// EnsureKubernetesGeneralConfiguration indicates an expected call of EnsureKubernetesGeneralConfiguration.
+// EnsureKubernetesGeneralConfiguration indicates an expected call of EnsureKubernetesGeneralConfiguration
 func (mr *MockEnsurerMockRecorder) EnsureKubernetesGeneralConfiguration(arg0, arg1, arg2, arg3 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "EnsureKubernetesGeneralConfiguration", reflect.TypeOf((*MockEnsurer)(nil).EnsureKubernetesGeneralConfiguration), arg0, arg1, arg2, arg3)
 }
 
-// EnsureVPNSeedServerDeployment mocks base method.
+// EnsureMachineControllerManagerDeployment mocks base method
+func (m *MockEnsurer) EnsureMachineControllerManagerDeployment(arg0 context.Context, arg1 context0.GardenContext, arg2, arg3 *v1.Deployment) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "EnsureMachineControllerManagerDeployment", arg0, arg1, arg2, arg3)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// EnsureMachineControllerManagerDeployment indicates an expected call of EnsureMachineControllerManagerDeployment
+func (mr *MockEnsurerMockRecorder) EnsureMachineControllerManagerDeployment(arg0, arg1, arg2, arg3 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "EnsureMachineControllerManagerDeployment", reflect.TypeOf((*MockEnsurer)(nil).EnsureMachineControllerManagerDeployment), arg0, arg1, arg2, arg3)
+}
+
+// EnsureMachineControllerManagerVPA mocks base method
+func (m *MockEnsurer) EnsureMachineControllerManagerVPA(arg0 context.Context, arg1 context0.GardenContext, arg2, arg3 *v11.VerticalPodAutoscaler) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "EnsureMachineControllerManagerVPA", arg0, arg1, arg2, arg3)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// EnsureMachineControllerManagerVPA indicates an expected call of EnsureMachineControllerManagerVPA
+func (mr *MockEnsurerMockRecorder) EnsureMachineControllerManagerVPA(arg0, arg1, arg2, arg3 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "EnsureMachineControllerManagerVPA", reflect.TypeOf((*MockEnsurer)(nil).EnsureMachineControllerManagerVPA), arg0, arg1, arg2, arg3)
+}
+
+// EnsureVPNSeedServerDeployment mocks base method
 func (m *MockEnsurer) EnsureVPNSeedServerDeployment(arg0 context.Context, arg1 context0.GardenContext, arg2, arg3 *v1.Deployment) error {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "EnsureVPNSeedServerDeployment", arg0, arg1, arg2, arg3)
@@ -219,13 +247,13 @@ func (m *MockEnsurer) EnsureVPNSeedServerDeployment(arg0 context.Context, arg1 c
 	return ret0
 }
 
-// EnsureVPNSeedServerDeployment indicates an expected call of EnsureVPNSeedServerDeployment.
+// EnsureVPNSeedServerDeployment indicates an expected call of EnsureVPNSeedServerDeployment
 func (mr *MockEnsurerMockRecorder) EnsureVPNSeedServerDeployment(arg0, arg1, arg2, arg3 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "EnsureVPNSeedServerDeployment", reflect.TypeOf((*MockEnsurer)(nil).EnsureVPNSeedServerDeployment), arg0, arg1, arg2, arg3)
 }
 
-// ShouldProvisionKubeletCloudProviderConfig mocks base method.
+// ShouldProvisionKubeletCloudProviderConfig mocks base method
 func (m *MockEnsurer) ShouldProvisionKubeletCloudProviderConfig(arg0 context.Context, arg1 context0.GardenContext, arg2 *semver.Version) bool {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "ShouldProvisionKubeletCloudProviderConfig", arg0, arg1, arg2)
@@ -233,7 +261,7 @@ func (m *MockEnsurer) ShouldProvisionKubeletCloudProviderConfig(arg0 context.Con
 	return ret0
 }
 
-// ShouldProvisionKubeletCloudProviderConfig indicates an expected call of ShouldProvisionKubeletCloudProviderConfig.
+// ShouldProvisionKubeletCloudProviderConfig indicates an expected call of ShouldProvisionKubeletCloudProviderConfig
 func (mr *MockEnsurerMockRecorder) ShouldProvisionKubeletCloudProviderConfig(arg0, arg1, arg2 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ShouldProvisionKubeletCloudProviderConfig", reflect.TypeOf((*MockEnsurer)(nil).ShouldProvisionKubeletCloudProviderConfig), arg0, arg1, arg2)
